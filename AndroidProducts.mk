5a58582d002b9bb6546d3f663b64d5f22da4821e
Pankaj Meharcdhandani <65183354+Killmonger007@users.noreply.github.com>
2022-06-14 13:29:41 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,21 +1,9 @@
-# Copyright (C) 2017-2020 The MarkOS Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+
 
 LOCAL_PATH := device/samsung/j7elte
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_PATH)/MarkOS_j7elte.mk
+    $(LOCAL_PATH)/havoc_j7elte.mk
 
 COMMON_LUNCH_CHOICES := \
     MarkOS_j7elte-user \
